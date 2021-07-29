5374b869f4b6e2b4aefcbd8efc02cd941d2deb24
mntalha <mntalha.kilic@gmail.com>
2021-07-29 12:42:29 +0300

    Added
    #get exact location
    print("Current directory ",os.getcwd())

--- tk_os_modules.py
+++ tk_os_modules.py
@@ -10,6 +10,11 @@ import  os
 #current file name
 print(os.path.basename(__file__))
 
+#get exact location
+## os.getcwd()
+print("Current directory ",os.getcwd())
+
+
 #list theall files unders directories name
 path = "C:/Users/talha/Desktop/"
 print(os.listdir(path))
